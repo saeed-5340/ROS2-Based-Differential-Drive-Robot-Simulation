dd82b697471a942fe82c348e1979ebada48ca46f
Saeed Ahamed Mridha <130834907+saeed-5340@users.noreply.github.com>
2026-03-12 02:24:24 +0600

    Uncomment rviz_node in gz_sim.launch.py

--- src/mobile_robot_description/launch/gz_sim.launch.py
+++ src/mobile_robot_description/launch/gz_sim.launch.py
@@ -79,5 +79,5 @@ def generate_launch_description():
         robot_state_publisher_node,
         # joint_state_publisher_node,
         gazebo_bridge_node,
-        # rviz_node,
+        rviz_node,
     ])
